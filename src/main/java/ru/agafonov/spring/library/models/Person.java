cf997bb08aac807a9ef471502485b6aa240008c3
User <User@DESKTOP-1HBKUBT>
2023-02-24 22:34:55 +0300

    использовал тип данных Date в классе Person

--- src/main/java/ru/agafonov/spring/library/models/Person.java
+++ src/main/java/ru/agafonov/spring/library/models/Person.java
@@ -1,7 +1,11 @@
 package ru.agafonov.spring.library.models;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.*;
 import javax.validation.constraints.*;
+import javax.xml.crypto.Data;
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -30,6 +34,15 @@ public class Person {
 
     @OneToMany(mappedBy = "owner")
     private List<Item> items;
+
+    @Column(name = "date_of_birth")
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    private Date dateOfBirth;
+
+    @Column(name = "created_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
 
     public Person(){}
 
@@ -69,6 +82,22 @@ public class Person {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
     }
 
     @Override
--- src/main/java/ru/agafonov/spring/library/services/PeopleService.java
+++ src/main/java/ru/agafonov/spring/library/services/PeopleService.java
@@ -6,6 +6,7 @@ import org.springframework.transaction.annotation.Transactional;
 import ru.agafonov.spring.library.models.Person;
 import ru.agafonov.spring.library.repositories.PeopleRepository;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -32,6 +33,7 @@ public class PeopleService {
 
     @Transactional
     public void save(Person person){
+        person.setCreatedAt(new Date());
         peopleRepository.save(person);
     }
 
