d35f4bf869321708d61c8ba9a0cf7a7dff1a1e79
User <User@DESKTOP-1HBKUBT>
2023-02-24 17:34:08 +0300

    Перевел проект на Data

--- src/main/java/ru/agafonov/spring/library/models/Item.java
+++ src/main/java/ru/agafonov/spring/library/models/Item.java
@@ -13,7 +13,7 @@ public class Item {
     private int id;
 
     @Column(name="item_name")
-    private String item_name;
+    private String itemName;
 
     @ManyToOne
     @JoinColumn(name = "person_id", referencedColumnName = "id")
@@ -22,15 +22,15 @@ public class Item {
     public Item(){}
 
     public Item(String item_name) {
-        this.item_name = item_name;
+        this.itemName = item_name;
     }
 
     public String getItem_name() {
-        return item_name;
+        return itemName;
     }
 
     public void setItem_name(String item_name) {
-        this.item_name = item_name;
+        this.itemName = item_name;
     }
 
     public Person getOwner() {
@@ -45,7 +45,7 @@ public class Item {
     public String toString() {
         return "Item{" +
                 "id=" + id +
-                ", item_name='" + item_name + '\'' +
+                ", item_name='" + itemName + '\'' +
                 '}';
     }
 }
